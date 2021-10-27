63d361e02010ed17ec0ede143f43bcb6d9e05954
ITSTIME <itstime0809@gmail.com>
2021-10-27 20:53:18 +0900

    [update] VGP project start 2021-10-27 [add] main page, onboarding page coded. [fix] image size, bottomnavigator,

---
+++ vgp_app/lib/main.dart
@@ -0,0 +1,71 @@
+import 'dart:async';
+import 'onboardingscreen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'VGP',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    /*Timer(
+        Duration(seconds: 0),
+        () => Navigator.pushReplacement(context,
+            MaterialPageRoute(builder: (context) => OnboardingScreen()))); */
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [
+              Color(0xFFC8E6C9),
+              Color(0xFFC8E6C9),
+              Color(0xFFA5D6A7), //
+              Color(0xFF81C784), // deep color
+            ],
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image(
+                image: AssetImage(
+                  'lib/images/vgplogo.png',
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
