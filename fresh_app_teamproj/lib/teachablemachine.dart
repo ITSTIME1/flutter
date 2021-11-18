623523499b6b4a7745fb70de3663603e8a8c7962
ITSTIME <itstime0809@gmail.com>
2021-11-18 18:03:20 +0900

    [Update] fresh_app [fix] onboarding screen code reafactoring [add] main.dart -> WidgetsFlutter binding.esnureinitialized [add] systemchrome [add] onboarding2data.dart [add] onboarding2.dart -> mainonboarding [add] appstyle.dart [add] loginpage.dart [add] size_configs.dart [add] onboarding2data.dart

---
+++ fresh_app_teamproj/lib/teachablemachine.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TeachableMachine extends StatefulWidget {
+  const TeachableMachine({Key? key}) : super(key: key);
+
+  @override
+  _TeachableMachineState createState() => _TeachableMachineState();
+}
+
+class _TeachableMachineState extends State<TeachableMachine> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
